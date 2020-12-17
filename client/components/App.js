a960ac84ec941d05e14e330291a1b4160d3cd70c
patriciadeboer <pdeboer05@gmail.com>
2020-12-17 16:51:58 -0600

    Creating initial react components

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Footer from "./Footer";
+import AddGrocery from "./AddGrocery";
+import GroceryList from "./GroceryList";
 
 const App = () => (
   <div className="app">
-    <h1>Hello World</h1>
+    <img src="groceries.png" alt="Groceries" width="500" />
+    <h1>Groceries List</h1>
+
+    <div className="list">
+      <AddGrocery />
+      <GroceryList groceries={[]}/>
+      <Footer />
+    </div>
   </div>
 );
 
---
+++ client/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    Placeholder for footer
+  </div>
+);
+
+export default Footer;
